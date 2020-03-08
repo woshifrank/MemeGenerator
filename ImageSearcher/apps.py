1a051d05201b11f828abdcf6ee149da3bf76b55f
woshifrank <frankyyj98@gmail.com>
2020-03-08 00:23:48 -0500

    Create the ImageSearcher and TextInserter app for the MemeGenerator. First static template set up. Starting to configuring the view

---
+++ ImageSearcher/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImagesearcherConfig(AppConfig):
+    name = 'ImageSearcher'
--- MemeGenerator/settings.py
+++ MemeGenerator/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ImageSearcher',
+    'TextInserter',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'MemeGenerator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
