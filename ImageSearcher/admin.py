507346a80bf5153aa51f243afc5e457aa4444c79
woshifrank <frankyyj98@gmail.com>
2020-03-12 17:26:36 -0400

    Set up the model for Photo and admin user

--- ImageSearcher/admin.py
+++ ImageSearcher/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from ImageSearcher.models import Photo
 # Register your models here.
+admin.site.register(Photo)
--- TextInserter/models.py
+++ TextInserter/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
 # Create your models here.
+class Photo(models.Model):
+    title = models.TextField(blank=True,null=True)
+    text = models.TextField(blank=True,null=True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format = 'JPEG',
+        options = {'quality' : 100},
+        blank = True,
+        null = True
+    )
